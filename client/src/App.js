c1adeaa9358e5ea030a800b7072180e5206832a2
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-01 18:24:25 +0300

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Versionskontrolle App</h1>
+      <p>Willkommen in der Versionskontrolle App!</p>
+    </div>
+  );
+};
+
+export default App;
