eff4182b299b4e61902d38419c1d674f1937d787
weeqeecombe50 <mynieagbona@hotmail.com>
2025-10-01 18:24:22 +0300

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/versionsmanagement', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Datenbank verbunden');
+}).catch(err => {
+  console.error('Datenbankverbindung fehlgeschlagen:', err);
+});
+
+// Beispielroute
+app.get('/', (req, res) => {
+  res.send('Willkommen zur Versionskontrolle App!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
